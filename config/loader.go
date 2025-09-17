dd8b0340bfeb4ff75db3987b3f6bf62ee1554dc9
haxbash <bluemaquinajp@gmail.com>
2025-09-17 18:17:17 -0300

    feat: Add config loading and saving functionality

---
+++ config/loader.go
@@ -0,0 +1,78 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+
+func getGlobalConfigPath() (string, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(home, GLOBAL_CONFIG_FILE), nil
+}
+
+func LoadGlobalConfig() Config {
+	cfg := GetDefaultConfig()
+	path, err := getGlobalConfigPath()
+	if err != nil {
+		fmt.Printf("Warning: could not get home directory: %v\n", err)
+		return cfg
+	}
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return cfg
+	}
+
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		fmt.Printf("Warning: reading global config: %v\n", err)
+		return cfg
+	}
+
+	if err := json.Unmarshal(data, &cfg); err != nil {
+		fmt.Printf("Warning: parsing global config: %v\n", err)
+	}
+
+	return cfg
+}
+
+func LoadProjectConfig() Config {
+	path := filepath.Join(".", PROJECT_CONFIG_FILE)
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return GetDefaultConfig()
+	}
+
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return GetDefaultConfig()
+	}
+
+	var cfg Config
+	if err := json.Unmarshal(data, &cfg); err != nil {
+		return GetDefaultConfig()
+	}
+	return cfg
+}
+
+
+func SaveGlobalConfig(cfg Config) error {
+	path, err := getGlobalConfigPath()
+	if err != nil {
+		return err
+	}
+	data, _ := json.MarshalIndent(cfg, "", "  ")
+	return ioutil.WriteFile(path, data, 0644)
+}
+
+
+func SaveProjectConfig(cfg Config) error {
+	path := filepath.Join(".", PROJECT_CONFIG_FILE)
+	data, _ := json.MarshalIndent(cfg, "", "  ")
+	return ioutil.WriteFile(path, data, 0644)
+}
